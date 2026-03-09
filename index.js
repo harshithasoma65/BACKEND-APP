d5087366399fee119ddd3716e0993ac27f0e4957
Harshitha <harshithasome65@gmail.com>
2026-03-09 13:48:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+import express from "express";
+import expressLayouts from "express-ejs-layouts";
+import session from "express-session";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import dbConnect from "./config/db.js";
+
+// import {productRouter} from "./routes/productRoute.js";
+import { storeRouter } from "./routes/storeRoute.js";
+const app = express();
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(
+  session({
+    secret: "secret key",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+app.use("/", storeRouter);
+// app.use("/auth", authRouter);
+// app.use("/products", productRouter);
+// app.use("/users", userRouter);
+const startServer = async () => {
+  await dbConnect();
+  app.listen(5000, () => {
+  console.log("Server Started");
+});
+
+};
+startServer()
+
+
+
+
+
+
+
