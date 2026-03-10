ae961194ccd113a3b885465c42f01e86aacde31b
Harshitha <harshithasome65@gmail.com>
2026-03-10 10:52:55 +0530

    updated layout footer

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,4 +2,4 @@ import { getProducts } from "../controllers/productController";
 import express from "express";
 const productRouter = express.Router();
 productRouter.get("/", getProducts);
-export default productRouter 
+export { productRouter };
