c3bdb85af9a61864cc2708bcea612564a1259fef
Harshitha <harshithasome65@gmail.com>
2026-03-10 11:59:26 +0530

    updated layout footer

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,7 @@
-import { getProducts ,addProduct} from "../controllers/productController.js";
+import { getProducts ,addProductForm,addProduct} from "../controllers/productController.js";
 import express from "express";
 const productRouter = express.Router();
 productRouter.get("/", getProducts);
 productRouter.get("/add",addProductForm);
+productRouter.post("/add", addProduct);
 export default productRouter;
