7252ca9bc53bec66b5a788b5f9537c38bbd27d95
Harshitha <harshithasome65@gmail.com>
2026-03-13 11:44:36 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,21 @@
+import jwt from "jsonwebtoken";
+const SECRET = "mysecret";
 const authenticateAdmin = (req, res, next) => {
-if (req.session.user) {
+  if (req.session.user) {
     next();
-} else {
+  } else {
     res.render("auth/login");
-}
+  }
 };
-
-export {authenticateAdmin}
+const authenticateUser = (req, res, next) => {
+  try {
+    const header = req.headers.authorization;
+    const token = header.split(" ")[1];
+    const user = jwt.verify(token, SECRET);
+    req.user = user;
+    next();
+  } catch (err) {
+    res.json({ error: "Invalid User" });
+  }
+};
+export { authenticateAdmin, authenticateUser };
